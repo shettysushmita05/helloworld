9f4d2b0c71bd415a1ee66e2c537438445baff914
ASUS <ASUS@LAPTOP-RDJVOKQN>
2023-09-25 22:52:15 +0530

    First commit

---
+++ src/classeseg/Hello.java
@@ -0,0 +1,33 @@
+package classeseg;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		Car car=new Car("closed","on","seated",10);
+		System.out.println(car.run());
+		
+		
+		
+	// classes adding functionality
+		
+//		Car car = new Car();
+//		car.setDoors("closed");
+//		car.setDriver("seated");
+//		car.setEngine("on");
+//		car.setSpeed(10);
+//		System.out.println(car.run());
+//		
+		
+		
+//		
+//		car.setSpeed(10);
+//		System.out.println(car.getSpeed());
+//		
+//		
+//		car.setDoors("closed");
+//		System.out.println(car.getDoors());
+		
+	}
+
+}
